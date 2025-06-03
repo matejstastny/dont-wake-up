1ce8b61627b2e655fcb463df56267e68cb5de2cd
Matěj Šťastný <matysta@outlook.com>
2025-06-03 15:03:59 -0700

    Init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Game Started");
+    }
+
+    void Update()
+    {
+    }
+}
