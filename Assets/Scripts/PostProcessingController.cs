b1c3e06bfe6fcd9e6a645b4cf33ab480333c026f
Matěj Šťastný <matysta@outlook.com>
2025-06-08 18:56:59 -0700

    Added hurt effect functionality

---
+++ Assets/Scripts/PostProcessingController.cs
@@ -0,0 +1,63 @@
+/*
+ * Author: Matěj Šťastný
+ * Date created: 6/8/2025
+ * GitHub link: https://github.com/matysta/dont-wake-up
+ */
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingController : MonoBehaviour
+{
+    [Header("References")]
+    private PostProcessVolume _volume;
+    private Vignette  _vignette;
+    
+    [Header("State")]
+    private float _intensity;
+    
+    // Start --------------------------------------------------------------------------------------------
+    
+    private void Start()
+    {
+        _volume = GetComponent<PostProcessVolume>();
+        _volume.profile.TryGetSettings(out _vignette);
+
+        if (!_vignette)
+        {
+            Debug.LogError("PostProcessVolume not found");
+        }
+        else
+        {
+            _vignette.enabled.Override(false);
+        }
+    }
+    
+    // Controllers --------------------------------------------------------------------------------------
+
+    public void TakeDamage()
+    {
+        StartCoroutine(nameof(TakeDamageRoutine));
+    }
+    
+    // Private --------------------------------------------------------------------------------------
+    
+    private IEnumerator TakeDamageRoutine()
+    {
+        _intensity = 0.4f;
+        _vignette.enabled.Override(true);
+        _vignette.intensity.Override(_intensity);
+        yield return new WaitForSeconds(0.4f);
+
+        while (_intensity > 0)
+        {
+            _intensity -= 0.01f;
+            if (_intensity < 0) _intensity = 0;
+            _vignette.intensity.Override(_intensity);
+            yield return new WaitForSeconds(0.07f);
+        }
+        
+        _vignette.enabled.Override(false);
+    }
+}
